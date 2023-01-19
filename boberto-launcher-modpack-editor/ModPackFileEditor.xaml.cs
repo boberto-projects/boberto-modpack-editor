30552cf4ef98c08f2a2d06e88328ca32b4f83f01
Roberto Paes <robertocpaes@gmail.com>
2023-01-19 06:32:32 -0300

    refenrece object for file list

--- boberto-launcher-modpack-editor/ModPackFileEditor.xaml.cs
+++ boberto-launcher-modpack-editor/ModPackFileEditor.xaml.cs
@@ -9,12 +9,13 @@ public partial class ModPackFileEditor : ContentPage
     public ObservableCollection<MinecraftFile> Files { get; set; }
     public ObservableCollection<MinecraftFile> ClientFiles { get; set; }
     public ObservableCollection<MinecraftFile> ServerFiles { get; set; }
-
+    public List<MinecraftFile> FilesToRemove { get; set; }
     public ModPackFileEditor(ModPack modpack)
     {
         Files = new ObservableCollection<MinecraftFile>();
         ClientFiles = new ObservableCollection<MinecraftFile>();
         ServerFiles = new ObservableCollection<MinecraftFile>();
+        FilesToRemove = new List<MinecraftFile>();
         InitializeComponent();
 
         CurrentModPack = modpack;
@@ -42,17 +43,74 @@ public partial class ModPackFileEditor : ContentPage
     }
     private void DragGestureRecognizer_DragStarting(object sender, DragStartingEventArgs e)
     {
-        var label = (sender as Element)?.Parent as Label;
-        e.Data.Properties.Add("Text", label.Text);
+        // var label = (sender as Element)?.Parent as Label;
+        var item = (sender as BindableObject).BindingContext as MinecraftFile;
+        e.Data.Properties.Add("Text", item);
     }
 
-    private void DropGestureRecognizer_Drop(object sender, DropEventArgs e)
+    private void DropGestureRecognizer_Drop_Server(object sender, DropEventArgs e)
     {
-        var data = e.Data.Properties["Text"].ToString();
+        // var item = (sender as BindableObject).BindingContext as MinecraftFile;
         var frame = (sender as Element)?.Parent as Frame;
-        frame.Content = new Label
+        var item = e.Data.Properties["Text"] as MinecraftFile;
+        if (item != null)
         {
-            Text = data
-        };
+            ServerFiles.Add(item);
+        }
+    }
+    private void DropGestureRecognizer_Drop_Client(object sender, DropEventArgs e)
+    {
+        // var item = (sender as BindableObject).BindingContext as MinecraftFile;
+        var frame = (sender as Element)?.Parent as Frame;
+        var item = e.Data.Properties["Text"] as MinecraftFile;
+        if (item != null)
+        {
+            ClientFiles.Add(item);
+        }
+    }
+    private void DropGestureRecognizer_Drop_Delete(object sender, DropEventArgs e)
+    {
+        // var item = (sender as BindableObject).BindingContext as MinecraftFile;
+        var frame = (sender as Element)?.Parent as Frame;
+        var item = e.Data.Properties["Text"] as MinecraftFile;
+        if (item != null && FilesToRemove.Contains(item) == false)
+        {
+            FilesToRemove.Add(item);
+            if (item.Enviroment.Equals(TypeEnviroment.Client))
+            {
+                ClientFiles.Remove(item);
+                return;
+            }
+            ServerFiles.Remove(item);
+        }
+    }
+    private async void OnAddServerFilesClicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private async void OnAddClientFilesClicked(object sender, EventArgs e)
+    {
+
+    }
+    private async void SaveClicked(object sender, EventArgs e)
+    {
+        foreach (var item in ClientFiles)
+        {
+            Utils.MoveFileToClient(item.FullPath, CurrentModPack.Directory);
+        }
+        foreach (var item in ServerFiles)
+        {
+            Utils.MoveFileToServer(item.FullPath, CurrentModPack.Directory);
+        }
+        foreach (var item in FilesToRemove)
+        {
+            File.Delete(item.FullPath);
+        }
+        await Navigation.PopAsync();
+    }
+    private async void CancelClicked(object sender, EventArgs e)
+    {
+        await Navigation.PopAsync();
     }
 }
--- boberto-launcher-modpack-editor/Utils.cs
+++ boberto-launcher-modpack-editor/Utils.cs
@@ -61,5 +61,61 @@ namespace boberto_launcher_modpack_editor
             }
             Directory.Delete(path, true);
         }
+        public static void MoveFileToClient(string path, string modPackDir)
+        {
+            var outputPath = Path.Combine(GetModPacksDir(), modPackDir, ClientFolder);
+            var file = new FileInfo(path);
+            var targetFilePath = Path.Combine(outputPath, file.Name);
+            file.CopyTo(targetFilePath, true);
+
+        }
+        public static void MoveFileToServer(string path, string modPackDir)
+        {
+            var outputPath = Path.Combine(GetModPacksDir(), modPackDir, ServerFolder);
+            var file = new FileInfo(path);
+            var targetFilePath = Path.Combine(outputPath, file.Name);
+            file.CopyTo(targetFilePath, true);
+        }
+
+        /// <summary>
+        /// https://learn.microsoft.com/en-us/dotnet/standard/io/how-to-copy-directories
+        /// </summary>
+        /// <param name="sourceDir"></param>
+        /// <param name="destinationDir"></param>
+        /// <param name="recursive"></param>
+        /// <exception cref="DirectoryNotFoundException"></exception>
+        public static void CopyDirectory(string sourceDir, string destinationDir, bool recursive)
+        {
+            // Get information about the source directory
+            var dir = new DirectoryInfo(sourceDir);
+
+            // Check if the source directory exists
+            if (!dir.Exists)
+                throw new Exception($"Diretório não encontrado: {dir.FullName}");
+
+            // Cache directories before we start copying
+            DirectoryInfo[] dirs = dir.GetDirectories();
+
+            // Create the destination directory
+            Directory.CreateDirectory(destinationDir);
+
+            // Get the files in the source directory and copy to the destination directory
+            foreach (FileInfo file in dir.GetFiles())
+            {
+                string targetFilePath = Path.Combine(destinationDir, file.Name);
+                file.CopyTo(targetFilePath, true);
+            }
+
+            // If recursive and copying subdirectories, recursively call this method
+            if (recursive)
+            {
+                foreach (DirectoryInfo subDir in dirs)
+                {
+                    string newDestinationDir = Path.Combine(destinationDir, subDir.Name);
+                    CopyDirectory(subDir.FullName, newDestinationDir, true);
+                }
+            }
+        }
+
     }
 }
